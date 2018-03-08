f45746c0fdabd0b70b1fc0bc418669887a807c7a
Alex Anthony <alex19427@gmail.com>
2018-03-08 15:45:43 +0530

    Update build.py

--- q03_stop_word_stemmer/build.py
+++ q03_stop_word_stemmer/build.py
@@ -1,7 +1,7 @@
 # Default imports
 from greyatomlib.nlp_day_01_project.q01_load_data.build import q01_load_data
 from nltk.corpus import stopwords
-
+import pandas as pd
 stop = set(stopwords.words('english'))
 from nltk.stem.porter import PorterStemmer
 
