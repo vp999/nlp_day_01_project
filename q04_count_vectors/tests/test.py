5a2140ae1ce98d435586cc20be7742ecc84bf348
Abhishek Subramanian <abhisheksubu92@gmail.com>
2018-03-09 16:51:54 +0530

    Update test.py

--- q04_count_vectors/tests/test.py
+++ q04_count_vectors/tests/test.py
@@ -3,32 +3,29 @@ from inspect import getfullargspec
 import warnings
 # warnings.filterwarnings("ignore")
 from ..build import q04_count_vectors
-from greyatomlib.nlp_day_01_project.q04_count_vectors.build import q04_count_vectors as act_sol
-
-import numpy as np
-
+from numpy.testing import assert_array_equal
 path = 'data/20news-bydate-train/'
-student_return = q02_tokenize(path)
-original_return = act_sol(path)
-
+a,b = q04_count_vectors(path)
 
 class Testing(unittest.TestCase):
 
     #  Check the arguements of the function
-
     def test_args(self):
         # Input parameters tests
-        args = getfullargspec(q02_tokenize)
-        self.assertEqual(len(args[0]), 1, "Expected arguments %d, Given %d" % (1, len(args[0])))
+        args = getfullargspec(q04_count_vectors)
+        self.assertEqual(len(args[0]), 4, "Expected arguments %d, Given %d" % (1, len(args[0])))
+
+        # check the defaults of the function
 
     def test_defaults(self):
-        args = getfullargspec(q02_tokenize)
-        self.assertEqual(args[3], None, "Expected default values do not match given default values")
+        args = getfullargspec(q04_count_vectors)
+        self.assertEqual(args[3], ((1, 2),0.5,2), "Expected default values do not match given default values")
+
 
     def test_return_4(self):
-        self.assertEqual(np.array(student_return[0]).shape, np.array(original_return[0]).shape,
+        self.assertEqual(a.shape,(2262, 72601),
                          "The return values do not match expected values")
 
     def test_return_4_2(self):
-        self.assertEqual(np.array(student_return[1]).shape, np.array(original_return[1]).shape,
+        self.assertEqual(b.shape,(9052, 72601),
                          "The return values do not match expected values")
